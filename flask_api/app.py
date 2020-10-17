4ecb2d7eb3c5fff9d94039382c29b7404d6a48f4
Fofana Moussa <mofanafoussa@gmail.com>
2020-10-17 23:14:18 +0200

    started modeling db

--- flask_api/app.py
+++ flask_api/app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from model import *
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///model.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 
 @app.route("/hey")
